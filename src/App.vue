a7dc08a59b46695840ee88e66c6980aa76bb5fbb
Danny Halbesma <dannyhalbesma@gmail.com>
2022-03-17 22:10:52 +0100

    Show name, gender and isAlive from feed in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,46 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{this.feedUrls.default}}
+    <HelloWorld msg="Winter is coming"/>
+    <pre>
+    {{ characters }}
+    </pre>
+    <ul v-for="character in characters" v-bind:key="character.id">
+      <li>Name: {{ character.name }}</li>
+      <li>Gender: {{ character.gender }}</li>
+      <li>Is alive: {{ character.died }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import axios from "axios";
+// import vueAxios from "vue-axios";
 
 export default {
   name: 'App',
+  data(){
+    return {
+      characters: undefined
+    }
+  },
+  mounted(){
+    axios
+      .get('https://anapioficeandfire.com/api/characters/')
+      .then((response)=>{
+        this.characters=response.data;
+        console.warn(response)
+      })
+  },
   components: {
     HelloWorld
   },
-  data(){
-    return {
-      xmlContent: null,
-      xmlError: false,
-      errorMsg: "",
-      axiosLoading: false,
-
-      feed: "default",
-      characters: false,
-      feedUrl: "",
-      feedUrls: {
-        default: 'https://anapioficeandfire.com/api/characters/'
-      }
-    }
-  },
   methods: {
-    loadData() {
-      this.xmlContent = null;
-      this.xmlError = false;
-
-      
-      this.axiosLoading = true;
-
-      axios
-        .get(this.feedUrl)
-        .then(response => {
-          let jsonObj = this.$x2js.xml2js(response.data);
-          this.characters = jsonObj;
-        })
-        .catch(error => {
-          this.xmlError = true;
-          this.errorMsg = error;
-        })
-        .finally(
-          
-          () => (this.axiosLoading = false)
-        );
-
-    }
+    
   },
 
   created() {
-    this.feedUrl = this.feedUrls[this.feed];
-    this.loadData();
+    
   }
   
 };
@@ -65,11 +48,6 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-align: left;
 }
 </style>
